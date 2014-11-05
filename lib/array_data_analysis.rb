3d4a4908624491eb466499fe850c033a0dbc6166
tophat8855 <hrfarley@gmail.com>
2014-11-04 20:43:38 -0800

    finished assignment, initial commit

--- lib/array_data_analysis.rb
+++ lib/array_data_analysis.rb
@@ -1,2 +1,61 @@
 class ArrayDataAnalysis
+
+  def initialize(data)
+    @data = data
+  end
+
+  def lowest_temperature
+    low_temp_array = []
+    @data.each {|day| low_temp_array << day[2]}
+    low_temp_array.min
+  end
+
+  def highest_temperature
+    high_temp_array = []
+    @data.each {|day| high_temp_array << day[1]}
+    high_temp_array.max
+  end
+
+  def day_of_lowest_temperature
+    low_temp_array = []
+    ordered_low_temps = []
+    answer = 0
+    @data.each do |date|
+      low_temp_array << date[2]
+      ordered_low_temps = low_temp_array.sort
+    end
+
+    @data.each do |date|
+      if date[2] == ordered_low_temps[0]
+        answer += date[0]
+      end
+    end
+    answer
+  end
+
+  def day_of_highest_temperature
+    high_temp_array = []
+    ordered_high_temps = []
+    answer = 0
+    @data.each do |date|
+      high_temp_array << date[1]
+      ordered_high_temps = high_temp_array.sort
+    end
+
+    @data.each do |date|
+      if date[1] == ordered_high_temps[-1]
+        answer += date[0]
+      end
+    end
+    answer
+  end
+
+  def average_temperatures
+    average_temp_array = []
+    @data.each do |date|
+      average_temp_array << (date[1] + date[2]) * 0.5
+    end
+    average_temp_array
+  end
+
 end
--- lib/class_data_analysis.rb
+++ lib/class_data_analysis.rb
@@ -1,2 +1,34 @@
 class ClassDataAnalysis
+  def initialize(data)
+    @data = data
+  end
+
+  def lowest_temperature
+    low_temp_array = []
+    @data.each {|day| low_temp_array << day.low}
+    low_temp_array.min
+  end
+
+  def highest_temperature
+    high_temp_array = []
+    @data.each { |day| high_temp_array << day.high}
+    high_temp_array.max
+  end
+
+  def day_of_lowest_temperature
+    @data.sort_by!{ |day| day.low}
+    @data[0].day
+  end
+
+  def day_of_highest_temperature
+    @data.sort_by!{ |day| day.high}
+    @data[-1].day
+  end
+
+  def average_temperatures
+    average_temp_array = []
+    @data.each {|date| average_temp_array << (date.high + date.low) * 0.5}
+    average_temp_array
+  end
+
 end
